7efd353d4639e5fd28e5e2826e4bf95dd2249c3c
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-07 01:26:40 +0530

    added themes

---
+++ client/src/utils/Themes.js
@@ -0,0 +1,19 @@
+export const darkTheme = {
+    bg:"#191d28",
+    bgLight: "#2e3241",
+    primary:"#68c3c4",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#121212",
+    button:"#5c5b5b",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#68c3c4",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
