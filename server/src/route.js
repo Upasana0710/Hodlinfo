10ced6d0ba71a8197fdbfce122752e0f8204717d
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-07 14:09:18 +0530

    changed route

--- server/src/route.js
+++ server/src/route.js
@@ -3,8 +3,8 @@ import {getFromApi,getPostedData, search} from './controller.js';
 
 const router = express.Router();
 
-router.get('/',getFromApi);
-router.get('/check',getPostedData);
 router.get('/search', search);
+router.get('/create',getFromApi);
+router.get('/check',getPostedData);
 
 export default router;
