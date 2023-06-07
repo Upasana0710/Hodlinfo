08d8b215792c0615f11dcf2ba99479fff9dd74fd
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-07 12:48:46 +0530

    added images

---
+++ client/src/components/data.js
@@ -0,0 +1,13 @@
+import wazir from '../images/wazirx.png';
+import bit from '../images/bitbns.dcee6cf4.png';
+import coin from '../images/coindcx.png';
+import zebpay from '../images/zebpay.png';
+import colodax from '../images/colodax.png';
+
+export const data=[
+   wazir,
+    bit,
+    coin,
+    zebpay,
+    colodax
+]
